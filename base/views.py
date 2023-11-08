4534e7ca356218ea3e76c0d97be37cc8ec11ade0
shalevhagshur <shalev.hagshur@gmail.com>
2023-11-08 14:17:56 +0200

    voodoo shit3

--- base/views.py
+++ base/views.py
@@ -45,8 +45,8 @@ def myCategories(req):
 def product_list(request):
     products = Product.objects.all()
     serializer = ProductSerializer(products, many=True)  # Serialize the data
-    return render(request, 'index.html', {'products': serializer.data})
-    
+    return render(request, '../Front/index.html', {'products': serializer.data})
+
 
 @api_view(['POST'])
 def register(req):
@@ -84,7 +84,7 @@ def login_view(request):
 
     if user is not None:
         login(request, user)
-        return render(request, 'login.html')  # Render the login template
+        return render(request, '../Front/login.html')  # Render the login template
 
     return Response("Invalid credentials", status=status.HTTP_401_UNAUTHORIZED)
 
